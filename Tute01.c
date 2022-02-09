144fa760b58016a0c2eeaf9506c8238c4dac74c8
Sayuri <99312315+IT21238758@users.noreply.github.com>
2022-02-09 06:27:00 +0000

    complete 1

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,24 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+ {
+ //Creat variables 
+  int mark1 , mark2 , total;
+  float average;
+
+  //Get input from user
+  printf("Enter mark 1 :");
+  scanf("%d",&mark1);
+
+  printf("Enter mark 2 :");
+  scanf("%d",&mark2);
   
+  total=mark1 + mark2 ;
+  average =total/2;
+
+  printf("average is %.2f", average);
+
   return 0;
 }
 
